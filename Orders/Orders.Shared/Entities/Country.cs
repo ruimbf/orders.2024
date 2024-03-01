a1de8141d5c8bfdfc4a508fb76b6dca3f18c2358
Rui Ferreira <ruif.sps@gmail.com>
2024-03-01 23:12:31 +0000

    create database

---
+++ Orders/Orders.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [MaxLength(100, ErrorMessage = "O campo {0} não pode ter mais de {1} caracteres.")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public string Name { get; set; } = null!;
+    }
+}
